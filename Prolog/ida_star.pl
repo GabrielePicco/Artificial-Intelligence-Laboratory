572bd5620deb5aca6225e82b5566976c234784cb
Gabriele Picco <piccogabriele@gmail.com>
2019-04-17 11:18:34 +0200

    Optimizing IDA*

--- Prolog/ida_star.pl
+++ Prolog/ida_star.pl
@@ -2,17 +2,19 @@ ida_star(Soluzione):-
     iniziale(S),
     precondizioni(S),
     euristica(S,H),
-    ida_star_aux(S,Soluzione,[S],H).
+    assertz(fvalue(H)),
+    %assertz(esc(true)),
+    ida_star_aux(S,Soluzione,[S],H),!.
 
 ida_star_aux(S,Soluzione,Visitati,FSoglia):-
     nl,write(fsoglia=FSoglia),
-    dfs_aux(S,Soluzione,Visitati,0,FSoglia),!.
+    dfs_aux(S,Soluzione,Visitati,0,FSoglia).
 
-ida_star_aux(_,_,_,FSoglia):-
-    retract(fvalue(FSoglia)), false.
-
-ida_star_aux(S,Soluzione,Visitati,FSoglia):-
-    min_nuova_FSoglia(FSoglia,NuovaFSoglia),
+ida_star_aux(S,Soluzione,Visitati,_):-
+    fvalue(NuovaFSoglia),
+    %esc(false),
+    %retract(esc(_)),
+    %assertz(esc(true)),
     ida_star_aux(S,Soluzione,Visitati,NuovaFSoglia).
 
 dfs_aux(S,[],_,_,_):-finale(S).
@@ -21,6 +23,9 @@ dfs_aux(S,_,_,G,FSoglia):-
     euristica(S,H),
     FValue is G + H,
     FValue > FSoglia,!,
+    fvalue(CurrentFValue),
+    (FValue < CurrentFValue ; CurrentFValue = FSoglia),
+    retract(fvalue(_)),
     assertz(fvalue(FValue)),
     false.
 
@@ -28,11 +33,8 @@ dfs_aux(S,[Azione|AzioniTail],Visitati,G,FSoglia):-
     applicabile(Azione,S),
     trasforma(Azione,S,SNuovo),
     \+member(SNuovo,Visitati),
+    %retract(esc(_)),
+    %assertz(esc(false)),
     costo(Azione,C),
     GNuovo is G+C,
     dfs_aux(SNuovo,AzioniTail,[SNuovo|Visitati],GNuovo,FSoglia).
-
-min_nuova_FSoglia(F,FSoglia):-
-    findall(V, (fvalue(V), V > F), ListaV),
-    min_list(ListaV, FSoglia).
-    

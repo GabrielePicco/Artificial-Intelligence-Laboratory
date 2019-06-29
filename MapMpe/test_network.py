e533749ace705a0287ecf4f5628223e66d94eb87
Gabriele Picco <piccogabriele@gmail.com>
2019-06-29 11:12:50 +0200

    add test small-medium networks

--- MapMpe/test_network.py
+++ MapMpe/test_network.py
@@ -14,5 +14,20 @@ def test_map():
     print(F"P(map,e): {prob}, MAP: {assgn}")
 
 
-test_mpe()
-test_map()
+#test_mpe()
+#test_map()
+
+def test_mpe_medium():
+    bn = parse_network("./sample_bayesian_networks/insurance.bif")
+    prob, assgn = mpe_ask(dict(PropCost="Thousand", RiskAversion="Adventurous"), bn)
+    print(F"P(mpe,e): {prob}, MPE: {assgn}")
+
+
+def test_map_medium():
+    bn = parse_network("./sample_bayesian_networks/insurance.bif")
+    prob, assgn = map_ask(dict(PropCost="Thousand", RiskAversion="Adventurous"), bn, map_vars=['Theft'])
+    print(F"P(map,e): {prob}, MAP: {assgn}")
+
+
+test_mpe_medium()
+test_map_medium()
